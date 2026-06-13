a921110204242f6de0fab55ddd9a2f4b523f6ad8
josue bondo <josuebondojw@gmail.com>
2026-06-13 20:10:04 +0100

    fix style app.index

--- app/Vues/app/index.php
+++ app/Vues/app/index.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet" />
     <style>
-        <style>body {
+        body {
             font-family: 'Manrope', sans-serif;
         }
 
